5fb0b48afbfa61545278be4c1d7480d422b36f9a
Hamed Fathi <HamedFathi@users.noreply.github.com>
2020-04-25 19:32:45 +0430

    Update XElementExtension.cs

--- Xml2CSharp/XElementExtension.cs
+++ Xml2CSharp/XElementExtension.cs
@@ -42,7 +42,7 @@ namespace Xml2CSharp
             foreach (var element in xElement.Elements().ToList())
             {
                 var tempClass = ElementToClass(element, classes);
-                var type = element.IsEmpty() ? "String" : tempClass.Name;
+                var type = element.IsEmpty() ? "string" : tempClass.Name;
 
                 yield return new Field
                 {
@@ -60,7 +60,7 @@ namespace Xml2CSharp
                 {
                     Name = attribute.Name.LocalName,
                     XmlName = attribute.Name.LocalName,
-                    Type = attribute.Value.GetType().Name,
+                    Type = attribute.Value.GetType().Name == "String" ? "string" : attribute.Value.GetType().Name,
                     XmlType = XmlType.Attribute,
                     Namespace = attribute.Name.NamespaceName
                 };
